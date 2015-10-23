4b9124f88dba6be473bd6204064b02f5c913f39d
jonathansayer <jon.sayer1@gmail.com>
2015-10-23 20:53:25 +0100

    added feature tests for selecting products and changing total

--- test/e2e/tillFeature.js
+++ test/e2e/tillFeature.js
@@ -1,9 +1,18 @@
 describe('Cafe Till', function() {
+  
   it('has a title', function() {
     browser.get('http://localhost:8080');
-
     expect(browser.getTitle()).toEqual('Cafe Till');
   });
 
+  it('the total is displayed at the start', function() {
+    browser.get('http://localhost:8080');
+    expect(element(by.binding('searchCtrl.total')).getText()).toEqual('£0.00')
+  })
 
+  it('changes the total when an item is selected', function() {
+    item_button = element(by.buttonText('Tea'));
+    item_button.click();
+    expect(element(by.binding('searchCtrl.total')).getText()).toEqual('£3.65')
+  })
 });
