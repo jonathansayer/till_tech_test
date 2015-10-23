b47ef4e3640c4fb95e6d9547914a67070b37cc16
jonathansayer <jon.sayer1@gmail.com>
2015-10-23 18:43:42 +0100

    changed feature test description to cafe till

--- test/e2e/tillFeature.js
+++ test/e2e/tillFeature.js
@@ -1,7 +1,9 @@
-describe('Github Profile finder', function() {
+describe('Cafe Till', function() {
   it('has a title', function() {
     browser.get('http://localhost:8080');
 
     expect(browser.getTitle()).toEqual('Cafe Till');
   });
+
+
 });
