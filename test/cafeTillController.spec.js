b92568c9bb7dc8f58bc16fa98cbece2a6ff927eb
jonathansayer <jon.sayer1@gmail.com>
2015-10-24 11:58:30 +0100

    reformated tests

--- test/cafeTillController.spec.js
+++ test/cafeTillController.spec.js
@@ -28,24 +28,23 @@ describe('cafeTillController', function() {
           }
         ]
 
-    prices =
-      {
-        "Cafe Latte": 4.75,
-        "Flat White": 4.75,
-        "Cappucino": 3.85,
-        "Single Espresso": 2.05,
-        "Double Espresso": 3.75,
-        "Americano": 3.75,
-        "Cortado": 4.55,
-        "Tea": 3.65,
-        "Choc Mudcake": 6.40,
-        "Choc Mousse": 8.20,
-        "Affogato": 14.80,
-        "Tiramisu": 11.40,
-        "Blueberry Muffin": 4.05,
-        "Chocolate Chip Muffin": 4.05,
-        "Muffin Of The Day": 4.55
-      }
+  prices = {
+            "Cafe Latte": 4.75,
+            "Flat White": 4.75,
+            "Cappucino": 3.85,
+            "Single Espresso": 2.05,
+            "Double Espresso": 3.75,
+            "Americano": 3.75,
+            "Cortado": 4.55,
+            "Tea": 3.65,
+            "Choc Mudcake": 6.40,
+            "Choc Mousse": 8.20,
+            "Affogato": 14.80,
+            "Tiramisu": 11.40,
+            "Blueberry Muffin": 4.05,
+            "Chocolate Chip Muffin": 4.05,
+            "Muffin Of The Day": 4.55
+          }
 
 
   var ctrl;
